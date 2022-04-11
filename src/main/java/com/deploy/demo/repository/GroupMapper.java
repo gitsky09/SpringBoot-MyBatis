d243ab0e545a40deb4a0ec9a0edcd4c909919ca3
CCT\The_A <theairofcloudflying@gmail.com>
2022-04-11 23:06:44 +0800

    註解部屬

--- src/main/java/com/deploy/demo/repository/GroupMapper.java
+++ src/main/java/com/deploy/demo/repository/GroupMapper.java
@@ -2,18 +2,28 @@ package com.deploy.demo.repository;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
 import com.deploy.demo.entity.GroupEntity;
 
 public interface GroupMapper {
 
+	@Insert("INSERT deploy_groups(NAME,CREATE_TIME,CREATE_USER) VALUES(#{name},#{crDateTime},#{crUser})")
 	public int insertGroup(GroupEntity group);
 
+	@Select("SELECT * FROM deploy_groups")
 	public List<GroupEntity> listGroup();
 
+	@Select("SELECT * FROM deploy_groups WHERE ID = #{id}")
 	public GroupEntity findByGroupId(int id);
 
+	@Update("UPDATE deploy_groups SET NAME = #{name},UPDATE_TIME = #{upDateTime}, UPDATE_USER = #{upUser} WHERE ID = #{id}")
 	public int updateGroup(GroupEntity group);
 
+	@Delete("DELETE FROM deploy_groups WHERE ID = #{id}")
 	public void deleteGroup(int id);
 	
 }
--- src/main/java/com/deploy/demo/repository/UserMapper.java
+++ src/main/java/com/deploy/demo/repository/UserMapper.java
@@ -2,19 +2,28 @@ package com.deploy.demo.repository;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
 import com.deploy.demo.entity.UserEntity;
 
 
 public interface UserMapper {
 
+	@Insert("INSERT INTO deploy_users( NAME, ACCOUNT, PASSWORD, SALT, PHONE, CREATE_TIME, CREATE_USER )VALUES( #{name}, #{account}, #{password}, #{salt}, #{phone}, #{crDateTime}, #{crUser})")
 	public int save(UserEntity user);
 	
+	@Select("SELECT deploy_users.name,deploy_users.account,deploy_users.phone FROM deploy_users")
 	public List<UserEntity> listUser();
 
+	@Select("SELECT * FROM deploy_users WHERE ID = #{id}")
 	public UserEntity findByUserId(int id);
 
+	@Update("UPDATE deploy_users SET NAME = #{name},ACCOUNT = #{account},PASSWORD = #{password},PHONE = #{phone},UPDATE_TIME = #{upDateTime}, UPDATE_USER = #{upUser} WHERE ID = #{id}")
 	public Integer updateUser(UserEntity user);
 	
-	public UserEntity findByAccount(String account);
-
+	@Select("SELECT deploy_users.account,deploy_users.password FROM deploy_users WHERE ACCOUNT = #{account}")
+	UserEntity findByAccount(String account);
+	
 }
